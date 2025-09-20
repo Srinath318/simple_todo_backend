806f99b719fafc2e6879c0942eb3bbbaa09458d5
Srinath318 <187srinath@gmail.com>
2025-09-20 23:07:38 +0530

    ready for railway deploy

--- src/main/java/com/example/ToDoApp/ToDoAppApplication.java
+++ src/main/java/com/example/ToDoApp/ToDoAppApplication.java
@@ -1,14 +1,26 @@
 package com.example.ToDoApp;
 
+import com.example.ToDoApp.entity.ToDo;
+import com.example.ToDoApp.repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class ToDoAppApplication implements CommandLineRunner {
+
+	@Autowired
+	private ToDoRepository todoRepository;
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ToDoAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Save a test Todo
+		todoRepository.save(new ToDo("Test task", false));
+		System.out.println("Todo saved!");
+	}
 }
---
+++ src/main/java/com/example/ToDoApp/entity/ToDo.java
@@ -0,0 +1,32 @@
+package com.example.ToDoApp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private boolean completed;
+
+    public ToDo() {} // default constructor
+
+    public ToDo(String title, boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+    // getters & setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public boolean isCompleted() { return completed; }
+    public void setCompleted(boolean completed) { this.completed = completed; }
+}
