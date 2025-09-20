01b8a417aa87fb9f4b7ede38259abee2ced93fc9
Srinath318 <187srinath@gmail.com>
2025-09-21 01:54:33 +0530

    ready for railway deploy with frontend main

--- src/main/java/com/example/ToDoApp/config/WebConfig.java
+++ src/main/java/com/example/ToDoApp/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://simple-todo-project.netlify.app/")
+                        .allowedOrigins("https://simple-todo-project.netlify.app")
                         .allowedMethods("*");
             }
         };
